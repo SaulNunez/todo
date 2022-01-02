02d6aa9a65c17f6fae09887a2b03f4e867faca10
Saul Nuñez <saul.nunez99@gmail.com>
2022-01-02 14:36:03 -0700

    Configure some CLI options

--- todo/Program.cs
+++ todo/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.CommandLine;
+
+var rootCommand = new RootCommand();
+
+var allOption = new Option("--all");
+allOption.AddAlias("-a");
+allOption.Description = "Show tasks in all lists";
+rootCommand.AddOption(allOption);
+var showCertainListOption = new Option("--list");
+showCertainListOption.AddAlias("-l");
+showCertainListOption.Description = "Show tasks of only this list";
+rootCommand.AddOption(showCertainListOption);
+var showListVerb = new Command("lists");
+rootCommand.AddCommand(showListVerb);
+showListVerb.Description = "Show all user lists";
+
+var addVerb = new Command("add");
+var listOption = new Option("--list");
+listOption.AddAlias("-l");
+listOption.Description = "Add a task to a certain list";
+rootCommand.AddOption(listOption);
+rootCommand.AddCommand(addVerb);
+var addToday = new Option("--in-my-day");
+addVerb.AddOption(addToday);
+var addNoteOption = new Option("--note");
+addNoteOption.AddAlias("-n");
+addVerb.AddOption(addNoteOption);
+
+var completeVerb = new Command("complete");
+var completeIndexOption = new Command("--index");
+completeIndexOption.AddAlias("-i");
+rootCommand.AddCommand(completeVerb);
