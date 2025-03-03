f0415ad54d4e7e175488ccf458fa2e023ad6a578
Saul Nunez <saul.nunez99@gmail.com>
2025-03-03 06:42:18 -0700

    Use PrettyPrint for the logic of how to print either the task list or the task to the screen

--- todo/PrettyPrint.cs
+++ todo/PrettyPrint.cs
@@ -1,25 +1,60 @@
+using System.Drawing;
 using Microsoft.Graph.Models;
+using Pastel;
 
 namespace todo;
 
 public static class PrettyPrint{
     public static void Print(TodoTask task){
         Console.WriteLine(task.Title);
+
+        Console.Write("Status: ");
+        Console.Write('\t');
         Console.WriteLine(task.Status);
+
         if(task.DueDateTime != null)
         {
-            Console.WriteLine(task.DueDateTime.ToDateTimeOffset());
+            Console.Write("Due Date:");
+            Console.Write('\t');
+            Console.WriteLine(DateOnly.FromDateTime(task.DueDateTime.ToDateTimeOffset().DateTime));
         }
         if(task.ReminderDateTime != null)
         {
-            Console.WriteLine(task.ReminderDateTime.ToDateTimeOffset());
+            Console.Write("Reminder Date: ");
+            Console.Write('\t');
+            Console.WriteLine(task.ReminderDateTime.ToDateTimeOffset().LocalDateTime);
+        }
+
+        Console.WriteLine("Notes:");
+        if(task.Body != null){
+            Console.WriteLine(task.Body?.Content);
         }
     }
 
     static void IndividualTaskItem(TodoTask task){
-        Console.Write(task.Status);
+        if(task.Status == Microsoft.Graph.Models.TaskStatus.Completed)
+        {
+            Console.Write("[x]");
+        }
+        else
+        {
+            Console.Write("[ ]");
+        }
         Console.Write('\t');
         Console.Write(task.Title);
+        if(task.DueDateTime != null)
+        {
+            Console.Write('\t');
+            if (task.DueDateTime.ToDateTime() < DateTime.Now)
+            {
+                Console.Write($"{task.DueDateTime.ToDateTime().ToString().Pastel(Color.Red)}");
+            } 
+            else
+            {
+                Console.Write(task.DueDateTime.ToDateTime());
+            }
+            
+        }
         Console.WriteLine();
     }
 
